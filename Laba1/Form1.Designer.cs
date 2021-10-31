ebaa8ed56ad2eabcd62fd17bbc592be91b1ce2e2
Volodymyr Matiishchuk <Volodymyr.matiishchuk@gmail.com>
2021-10-31 17:26:11 +0300

    Add project files.

---
+++ Laba1/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.startStop = new System.Windows.Forms.Button();
+            this.savePicture = new System.Windows.Forms.Button();
+            this.toBmp = new System.Windows.Forms.Button();
+            this.toTxt = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.useMedianFilterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(198, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(216, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(790, 399);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // startStop
+            // 
+            this.startStop.BackColor = System.Drawing.Color.Snow;
+            this.startStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startStop.Location = new System.Drawing.Point(12, 61);
+            this.startStop.Name = "startStop";
+            this.startStop.Size = new System.Drawing.Size(198, 33);
+            this.startStop.TabIndex = 4;
+            this.startStop.Text = "Start";
+            this.startStop.UseVisualStyleBackColor = false;
+            this.startStop.Click += new System.EventHandler(this.startStop_Click);
+            // 
+            // savePicture
+            // 
+            this.savePicture.BackColor = System.Drawing.Color.Snow;
+            this.savePicture.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.savePicture.Location = new System.Drawing.Point(12, 100);
+            this.savePicture.Name = "savePicture";
+            this.savePicture.Size = new System.Drawing.Size(198, 33);
+            this.savePicture.TabIndex = 6;
+            this.savePicture.Text = "Save bmp";
+            this.savePicture.UseVisualStyleBackColor = false;
+            this.savePicture.Click += new System.EventHandler(this.savePicture_Click);
+            // 
+            // toBmp
+            // 
+            this.toBmp.BackColor = System.Drawing.Color.Snow;
+            this.toBmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toBmp.Location = new System.Drawing.Point(12, 178);
+            this.toBmp.Name = "toBmp";
+            this.toBmp.Size = new System.Drawing.Size(198, 33);
+            this.toBmp.TabIndex = 7;
+            this.toBmp.Text = "Convert into .txt";
+            this.toBmp.UseVisualStyleBackColor = false;
+            this.toBmp.Click += new System.EventHandler(this.toBmp_Click);
+            // 
+            // toTxt
+            // 
+            this.toTxt.BackColor = System.Drawing.Color.Snow;
+            this.toTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toTxt.Location = new System.Drawing.Point(12, 217);
+            this.toTxt.Name = "toTxt";
+            this.toTxt.Size = new System.Drawing.Size(198, 33);
+            this.toTxt.TabIndex = 8;
+            this.toTxt.Text = "Convert from .txt";
+            this.toTxt.UseVisualStyleBackColor = false;
+            this.toTxt.Click += new System.EventHandler(this.toTxt_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Snow;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(12, 139);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(198, 33);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Clear";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1018, 28);
+            this.menuStrip2.TabIndex = 14;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 24);
+            this.fileToolStripMenuItem.Text = " File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Text = "Save as";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filterToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // filterToolStripMenuItem
+            // 
+            this.filterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.useMedianFilterToolStripMenuItem});
+            this.filterToolStripMenuItem.Name = "filterToolStripMenuItem";
+            this.filterToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.filterToolStripMenuItem.Text = "Filter";
+            // 
+            // useMedianFilterToolStripMenuItem
+            // 
+            this.useMedianFilterToolStripMenuItem.Name = "useMedianFilterToolStripMenuItem";
+            this.useMedianFilterToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.useMedianFilterToolStripMenuItem.Text = "Use median filter";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1018, 456);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.toTxt);
+            this.Controls.Add(this.toBmp);
+            this.Controls.Add(this.savePicture);
+            this.Controls.Add(this.startStop);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.menuStrip2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MZKIT_PROJECT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button startStop;
+        private System.Windows.Forms.Button savePicture;
+        private System.Windows.Forms.Button toBmp;
+        private System.Windows.Forms.Button toTxt;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem useMedianFilterToolStripMenuItem;
+    }
+}
+
---
+++ Laba1/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+namespace Laba1
+{
+    public partial class Form1 : Form
+    {
+        public string InputFileName = "";
+        public string OutputFileName = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private FilterInfoCollection CaptureDevice;
+        private VideoCaptureDevice FinalFrame;
+        private bool IsStart = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in CaptureDevice)
+            {
+                comboBox1.Items.Add(Device.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+            FinalFrame = new VideoCaptureDevice();
+        }
+
+        private void startStop_Click(object sender, EventArgs e)
+        {
+            if (!IsStart)
+            {
+                IsStart = !IsStart;
+                startStop.Text = "Stop";
+                FinalFrame = new VideoCaptureDevice(CaptureDevice[comboBox1.SelectedIndex].MonikerString);
+                FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+                FinalFrame.Start();
+            }
+            else
+            {
+                IsStart = !IsStart;
+                startStop.Text = "Start";
+                FinalFrame.Stop();
+            }
+        }
+        private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+        private void clear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+        private void savePicture_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                SaveFileDialog save = new SaveFileDialog();
+                save.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+                save.Title = "Save file .bmp";
+                save.DefaultExt = ".bmp";
+                save.Filter = "File BMP|*.bmp";
+                save.ShowDialog();
+                OutputFileName = save.FileName;
+                pictureBox1.Image.Save(OutputFileName, ImageFormat.Bmp);
+                MessageBox.Show("Image saved successfully");
+            }
+            else MessageBox.Show("File not found!");
+        }
+        private void toTxt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+            open.Title = "Select a file .txt";
+            open.Multiselect = false;
+            open.DefaultExt = ".txt";
+            open.Filter = "File TXT|*.txt";
+            open.FilterIndex = 0;
+
+            open.ShowDialog();
+
+            InputFileName = open.FileName;
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+            save.Title = "Save file .bmp";
+            save.DefaultExt = ".bmp";
+            save.Filter = "File BMP|*.bmp";
+            save.ShowDialog();
+
+            OutputFileName = save.FileName;
+            TxtToBmp();
+            Image image = Image.FromFile(OutputFileName);
+            pictureBox1.Image = image;
+        }
+        private void toBmp_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+            open.Title = "Select a file .BMP";
+            open.Multiselect = false;
+            open.DefaultExt = ".bmp";
+            open.Filter = "File BMP|*.bmp";
+            open.FilterIndex = 0;
+
+            open.ShowDialog();
+
+            InputFileName = open.FileName;
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+            save.Title = "Save file .txt";
+            save.DefaultExt = ".txt";
+            save.Filter = "File TXT|*.txt";
+            save.ShowDialog();
+
+            OutputFileName = save.FileName;
+            Image image = Image.FromFile(InputFileName);
+            pictureBox1.Image = image;
+            BmpToTxt(pictureBox1.Image);
+        }
+        private void BmpToTxt(Image image)
+        {
+            string stringPartOfColor;
+            Cursor = Cursors.WaitCursor;
+            using (Bitmap bmp = new Bitmap(image))
+            {
+                using (StreamWriter sw = new StreamWriter(OutputFileName, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(bmp.Width);
+                    sw.WriteLine(bmp.Height);
+
+                    for (int y = 0; y < bmp.Height; y++)
+                    {
+                        for (int x = 0; x < bmp.Width; x++)
+                        {
+                            Color pxl = bmp.GetPixel(x, y);
+                            stringPartOfColor = Convert.ToString(pxl.R, 2).PadLeft(8, '0');
+                            sw.WriteLine(stringPartOfColor);
+                            stringPartOfColor = Convert.ToString(pxl.G, 2).PadLeft(8, '0');
+                            sw.WriteLine(stringPartOfColor);
+                            stringPartOfColor = Convert.ToString(pxl.B, 2).PadLeft(8, '0');
+                            sw.WriteLine(stringPartOfColor);
+                        }
+                    }
+                    sw.Close(); 
+                }
+            }
+            Cursor = Cursors.Default;
+        }
+        private void TxtToBmp()
+        {
+            Cursor = Cursors.WaitCursor;
+            Stream imageStream = new MemoryStream();
+            int width, height;
+            int r, g, b;
+
+            using (StreamReader sr = new StreamReader(InputFileName, System.Text.Encoding.Default))
+            {
+                width = Convert.ToInt16(sr.ReadLine());
+                height = Convert.ToInt16(sr.ReadLine());
+                using (Bitmap bmp = new Bitmap(width, height))
+                {
+                    for (int y = 0; y < bmp.Height; y++)
+                    {
+                        for (int x = 0; x < bmp.Width; x++)
+                        {
+                            r = Convert.ToInt32(sr.ReadLine(), 2);
+                            g = Convert.ToInt32(sr.ReadLine(), 2);
+                            b = Convert.ToInt32(sr.ReadLine(), 2);
+                            bmp.SetPixel(x, y, Color.FromArgb(r, g, b));
+                        }
+                    }
+                    bmp.Save(OutputFileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+                sr.Close();
+            }
+            Cursor = Cursors.Default;
+
+        }
+        private void MedianFiltering_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+            open.Title = "Select a file .BMP";
+            open.Multiselect = false;
+            open.DefaultExt = ".bmp";
+            open.Filter = "File BMP|*.bmp";
+            open.FilterIndex = 0;
+
+            open.ShowDialog();
+
+            InputFileName = open.FileName;
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+            save.Title = "Save file .BMP";
+            save.DefaultExt = ".bmp";
+            save.Filter = "File BMP|*.bmp";
+            save.ShowDialog();
+
+            OutputFileName = save.FileName;
+            Image image = Image.FromFile(InputFileName);
+            Bitmap bmp = new Bitmap(image);
+            pictureBox1.Image = bmp;
+
+        }
+    }
+}
